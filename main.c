196e11257d9fb57f05ee42166daff938e4f122a5
Andreas Olsson <andreasolsson2020@outlook.com>
2023-08-10 14:11:07 +0200

    create choosenumbers function

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+void menu() {
+    const char *menu[] = {"Add", "Subtract", "Multiply","Divide","Exit"};
+        int menuSize = sizeof(menu) / sizeof(menu[0]);
+
+        for (int i = 0; i < menuSize;i++) {
+            printf("%d %s\n", i + 1, menu[i]);
+            printf("--------------------------------\n");
+        }
+}
+
+void chooseNumbers(int *num1, int *num2) {
+    printf("Enter two numbers\n");
+    scanf("%d %d", num1, num2);
+}
+
 
 int main()
 {
-    const char *menu[] = {"Add", "Subtract", "Multiply","Divide","Exit"};
-    int menuSize = sizeof(menu) / sizeof(menu[0]);
+    int num1, num2;
+    // Show menu
+    menu();
 
-    for (int i = 0; i < menuSize;i++) {
-        printf("%d %s\n", i + 1, menu[i]);
-        printf("--------------------------------\n");
-    }
+    // Choose numbers
+    chooseNumbers(&num1, &num2);
+
+    printf("You have chosen: %d and %d\n", num1, num2);
     return 0;
 }
