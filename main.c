8fdf54ada30739da64b0b5712eda83086a0e649c
Andreas Olsson <andreasolsson2020@outlook.com>
2023-08-10 14:17:05 +0200

    create chooseMenuNumber function

--- main.c
+++ main.c
@@ -15,16 +15,27 @@ void chooseNumbers(int *num1, int *num2) {
     scanf("%d %d", num1, num2);
 }
 
-
+void chooseMenuNumber(int *menuNumber) {
+    printf("Choose a number from the menu");
+    scanf("%d",menuNumber);
+}
 int main()
 {
-    int num1, num2;
+    int num1, num2, menuNumber;
     // Show menu
     menu();
+
+    // Choose menu number
+    chooseMenuNumber(&menuNumber);
+
+    printf("You have chosen menu number: %d", menuNumber);
 
     // Choose numbers
     chooseNumbers(&num1, &num2);
 
-    printf("You have chosen: %d and %d\n", num1, num2);
+    //printf("You have chosen: %d and %d\n", num1, num2);
+
+    //Choose menu number
+
     return 0;
 }
